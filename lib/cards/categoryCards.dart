749342a6fb43a3ee23cc3d58113c5b654998bb23
Kyle Bradshaw <kyle@hotfixit.net>
2017-06-05 16:51:23 -0500

    Replace broken SnackBar with AlertDialog

--- lib/cards/categoryCards.dart
+++ lib/cards/categoryCards.dart
@@ -25,7 +25,24 @@ void showCategoryCard(BuildContext context, String category, String objID,
       );
       break;
     default:
-      Scaffold.of(context).showSnackBar(new SnackBar(
-          content: new Text("This category is not available yet.")));
+      showDialog(
+        context: context,
+        child: new AlertDialog(
+          content: new Text("This category ($category) is not implemented yet."),
+          actions: <Widget>[new RaisedButton(
+            child: new Text("Ok"),
+            onPressed: (){
+              Navigator.pop(context);
+            },
+          )],
+        )
+      );
   }
+}
+
+void showPackageCard(BuildContext context, Map<String, dynamic> data){
+  showDialog(
+    context: context,
+    child: new PackageInfoCard(packageData: data)
+  );
 }
