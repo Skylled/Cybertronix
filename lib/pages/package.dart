15ecbaf0fb50c148ca1cef8c8ba92c6a474d5b55
Kyle Bradshaw <kyle@hotfixit.net>
2017-10-27 12:08:03 -0500

    Fun and effective refactor to Package Creator page

--- lib/pages/package.dart
+++ lib/pages/package.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../drawer.dart';
 import '../cards/creatorCards.dart';
 
+// Cheers to https://flutter.io/catalog/samples/tabbed-app-bar/
+
 class PackageCreatorPage extends StatefulWidget {
   final Map<String, dynamic> initialData;
 
@@ -14,6 +16,7 @@ class PackageCreatorPage extends StatefulWidget {
 class _PackageCreatorPageState extends State<PackageCreatorPage> {
   List<Widget> children;
   Map<String, dynamic> currentData;
+  bool saved = false;
 
   List<String> components = <String>["panel", "pump", "motor", "jockeypanel", "jockeypump"];
 
@@ -36,27 +39,62 @@ class _PackageCreatorPageState extends State<PackageCreatorPage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Package Creator")
-      ),
-      persistentFooterButtons: <Widget>[
-        new FlatButton(
-          child: new Text("Cancel"),
-          onPressed: (){ Navigator.pop(context); },
+    return new DefaultTabController(
+      length: components.length,
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Package Creator"),
+          bottom: new TabBar(
+            isScrollable: true,
+            tabs: components.map((String component) {
+              return new Tab(text: component);
+            }).toList(),
+          ),
         ),
-        new FlatButton(
-          child: new Text("Save & Finish"),
-          textColor: Theme.of(context).accentColor,
-          onPressed: (){
-            Navigator.pop(context, currentData);
+        persistentFooterButtons: <Widget>[
+          new FlatButton(
+            child: new Text("Cancel"),
+            onPressed: (){ Navigator.pop(context); },
+          ),
+          new FlatButton(
+            child: new Text("Save & Finish"),
+            textColor: Theme.of(context).accentColor,
+            onPressed: (){
+              saved = true;
+              Navigator.pop(context, currentData);
+            },
+          ),
+        ],
+        drawer: buildDrawer(context, 'creator'),
+        body: new WillPopScope(
+          onWillPop: () async {
+            if (saved) return true;
+            return await showDialog<bool>(
+              context: context,
+              child: new SimpleDialog(
+                title: new Text("Your changes have not been saved.\nAre you sure you'd like to leave this page?"),
+                children: <Widget>[
+                  new SimpleDialogOption(
+                    onPressed: () { Navigator.pop(context, true); },
+                    child: new Text("Yes"),
+                  ),
+                  new SimpleDialogOption(
+                    onPressed: (){ Navigator.pop(context, false); },
+                    child: new Text("No"),
+                  ),
+                ],
+              )
+            );
           },
+          child: new TabBarView(
+            children: components.map((String component){
+              return new Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: getComponentCard(component, currentData["power"], changeData),
+              );
+            }).toList(),
+          ),
         ),
-      ],
-      drawer: buildDrawer(context, 'creator'),
-      body: new ListView(
-        // TODO: WillPopScope
-        children: new List<Widget>.from(children),
       ),
     );
   }
