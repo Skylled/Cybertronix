82d4ee04941ca034f1fab9672921a331fd169c40
Kyle Bradshaw <kyle@hotfixit.net>
2017-12-18 13:27:35 -0600

    Initial work on Pump creator

--- lib/cards/creator/package/pump.dart
+++ lib/cards/creator/package/pump.dart
@@ -23,7 +23,209 @@ class _PumpCreatorCardState extends State<PumpCreatorCard> {
 
   List<CreatorItem<dynamic>> getPumpItems(){
     return <CreatorItem<dynamic>>[
-      // TODO: Fill in!
+      new CreatorItem<String>( // Manufacturer
+        name: "Manufacturer",
+        value: widget.initialData["manufacturer"] ?? '',
+        hint: "Aurora",
+        valueToString: (String value) => value,
+        builder: (CreatorItem<String> item){
+          void close(){
+            setState((){
+              item.isExpanded = false;
+            });
+          }
+
+          return new Form(
+            child: new Builder(
+              builder: (BuildContext context){
+                return new CollapsibleBody(
+                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  onSave: () { Form.of(context).save(); close(); },
+                  onCancel: () { Form.of(context).reset(); close(); },
+                  child: new Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: new TextFormField(
+                      controller: item.textController,
+                      decoration: new InputDecoration(
+                        hintText: item.hint,
+                        labelText: item.name,
+                      ),
+                      onSaved: (String value){
+                        item.value = value;
+                        currentData['manufacturer'] = value;
+                        widget.changeData('pump', currentData);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      ),
+      new CreatorItem<String>( // Model #
+        name: "Model #",
+        value: widget.initialData["model"] ?? '',
+        valueToString: (String value) => value,
+        builder: (CreatorItem<String> item){
+          void close(){
+            setState((){
+              item.isExpanded = false;
+            });
+          }
+
+          return new Form(
+            child: new Builder(
+              builder: (BuildContext context){
+                return new CollapsibleBody(
+                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  onSave: () { Form.of(context).save(); close(); },
+                  onCancel: () { Form.of(context).reset(); close(); },
+                  child: new Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: new TextFormField(
+                      controller: item.textController,
+                      decoration: new InputDecoration(
+                        hintText: item.hint,
+                        labelText: item.name,
+                      ),
+                      onSaved: (String value){
+                        item.value = value;
+                        currentData['model'] = value;
+                        widget.changeData('pump', currentData);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+      new CreatorItem<String>( // Serial #
+        name: "Serial #",
+        value: widget.initialData["serial"] ?? '',
+        valueToString: (String value) => value,
+        builder: (CreatorItem<String> item){
+          void close(){
+            setState((){
+              item.isExpanded = false;
+            });
+          }
+
+          return new Form(
+            child: new Builder(
+              builder: (BuildContext context){
+                return new CollapsibleBody(
+                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  onSave: () { Form.of(context).save(); close(); },
+                  onCancel: () { Form.of(context).reset(); close(); },
+                  child: new Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: new TextFormField(
+                      controller: item.textController,
+                      decoration: new InputDecoration(
+                        hintText: item.hint,
+                        labelText: item.name,
+                      ),
+                      onSaved: (String value){
+                        item.value = value;
+                        currentData['serial'] = value;
+                        widget.changeData('pump', currentData);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+      new CreatorItem<int>( // RPM
+        name: "RPM",
+        value: widget.initialData["rpm"],
+        hint: "(e.g. 3400)",
+        valueToString: (int value) => value != null ? value.toString() : '',
+        builder: (CreatorItem<int> item){
+          void close(){
+            setState((){
+              item.isExpanded = false;
+            });
+          }
+
+          return new Form(
+            child: new Builder(
+              builder: (BuildContext context){
+                return new CollapsibleBody(
+                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  onSave: () { Form.of(context).save(); close(); },
+                  onCancel: () { Form.of(context).reset(); close(); },
+                  child: new Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: new TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: item.textController,
+                      decoration: new InputDecoration(
+                        hintText: item.hint,
+                        labelText: item.name,
+                      ),
+                      onSaved: (String value){
+                        int rpm = int.parse(value, onError: (String input) => null);
+                        item.value = rpm;
+                        currentData["rpm"] = rpm;
+                        widget.changeData('pump', currentData);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      ),
+      new CreatorItem<num>( // Shutoff pressure
+        name: "Shutoff pressure",
+        value: widget.initialData["shutoff"],
+        valueToString: (num value) => value != null ? value.toString() : '',
+        builder: (CreatorItem<num> item) {
+          void close(){
+            setState((){
+              item.isExpanded = false;
+            });
+          }
+
+          return new Form(
+            child: new Builder(
+              builder: (BuildContext context){
+                return new CollapsibleBody(
+                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  onSave: () { Form.of(context).save(); close(); },
+                  onCancel: () { Form.of(context).reset(); close(); },
+                  child: new Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: new TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: item.textController,
+                      decoration: new InputDecoration(
+                        labelText: item.name
+                      ),
+                      onSaved: (String value){
+                        num psi = num.parse(value, (String input) => null);
+                        item.value = psi;
+                        currentData['shutoff'] = psi;
+                        widget.changeData('pump', currentData);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      ),
+      // These next few are a GPM int at a PSI int.
+      // TODO: Capacity [GPM] @ [PSI]
+      // TODO: 150% [GPM] @ [PSI]
     ];
   }
 
